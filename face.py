004992c533098b482a63b2ced109387bcfe4ea7d
Mayank Soni <15159417+mayanxoni@users.noreply.github.com>
2019-11-15 00:26:59 +0530

    Create dataset for a particular face

--- face.py
+++ face.py
@@ -5,14 +5,20 @@ classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cam = cv2.VideoCapture(0)
 
+id = input("Enter User ID: ")
+ver = 0
 while(True):
     ret, img = cam.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = classifier.detectMultiScale(gray, 1.3, 5)
     for(x, y, w, h) in faces:
+        ver = ver + 1
+        cv2.imwrite("dataSet/user_"+str(id)+"_version_"+str(ver)+".jpg", gray[y:y+h, x:x+w])
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.waitKey(100)
     cv2.imshow("Face", img)
-    if(cv2.waitKey(1) == ord('q')):
+    cv2.waitKey(1)
+    if(ver > 20):
         break
 
 cam.release()
