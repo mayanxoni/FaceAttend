34a0e39127a386a7043a3c85dc6c59a339e3779a
Mayank Soni <15159417+mayanxoni@users.noreply.github.com>
2019-11-16 23:55:56 +0530

    Long term cleanup

--- recognizer.py
+++ recognizer.py
@@ -1,33 +1,30 @@
 import cv2
 import os
 import sqlite3
-import pickle
-from PIL import Image
-import numpy as np
 
 
 def assure_path_exists(path):
-    dir = os.path.dirname(path)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 assure_path_exists("trainer/")
-recognizer.read('trainer/trainer.yml')
+recognizer.read("trainer/trainer.yml")
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
 
-def getProfile(student_id):
-    conn = sqlite3.connect("facebase.db")
+def get_profile(student_id):
+    conn = sqlite3.connect("database/facebase.db")
     cmd = "SELECT * FROM students WHERE ID = " + str(student_id)
     cursor = conn.execute(cmd)
-    profile = None
+    student_profile = None
     for row in cursor:
-        profile = row
+        student_profile = row
     conn.close()
-    return profile
+    return student_profile
 
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -40,7 +37,7 @@ while True:
     for (x, y, w, h) in faces:
         Id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
         cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        profile = getProfile(Id)
+        profile = get_profile(Id)
         if profile is not None:
             cv2.putText(im, str(profile[0]), (x, y + h + 30), font, 1, (0, 0, 255), 3)
             cv2.putText(im, str(profile[1]), (x, y + h + 60), font, 1, (0, 0, 255), 3)
