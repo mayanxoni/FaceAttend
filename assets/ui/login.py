43f9448be7255f649386f08b35eefdc980c558d7
Mayank Soni <mayanksoni282@gmail.com>
2019-12-03 17:56:01 +0530

    Add py files for ui

---
+++ assets/ui/login.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'login.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_form_login(object):
+    def setupUi(self, form_login):
+        form_login.setObjectName("form_login")
+        form_login.setWindowModality(QtCore.Qt.ApplicationModal)
+        form_login.setEnabled(True)
+        form_login.resize(380, 420)
+        form_login.setFixedSize(380, 420)
+        form_login.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../icons/FaceAttend2.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        form_login.setWindowIcon(icon)
+        form_login.setLayoutDirection(QtCore.Qt.LeftToRight)
+        form_login.setAutoFillBackground(False)
+        form_login.setTabShape(QtWidgets.QTabWidget.Rounded)
+        form_login.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
+        form_login.setUnifiedTitleAndToolBarOnMac(False)
+        self.central_widget = QtWidgets.QWidget(form_login)
+        self.central_widget.setObjectName("central_widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.central_widget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.grid_layout = QtWidgets.QGridLayout()
+        self.grid_layout.setObjectName("grid_layout")
+        self.label_spacer_top = QtWidgets.QLabel(self.central_widget)
+        self.label_spacer_top.setText("")
+        self.label_spacer_top.setObjectName("label_spacer_top")
+        self.grid_layout.addWidget(self.label_spacer_top, 0, 0, 1, 1)
+        self.gridLayout.addLayout(self.grid_layout, 0, 0, 1, 1)
+        self.button_signup = QtWidgets.QPushButton(self.central_widget)
+        self.button_signup.setFocusPolicy(QtCore.Qt.StrongFocus)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("../icons/key.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.button_signup.setIcon(icon1)
+        self.button_signup.setObjectName("button_signup")
+        self.gridLayout.addWidget(self.button_signup, 9, 0, 1, 1)
+        self.line_edit_password = QtWidgets.QLineEdit(self.central_widget)
+        self.line_edit_password.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.line_edit_password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.line_edit_password.setAlignment(QtCore.Qt.AlignCenter)
+        self.line_edit_password.setClearButtonEnabled(True)
+        self.line_edit_password.setObjectName("line_edit_password")
+        self.gridLayout.addWidget(self.line_edit_password, 4, 0, 1, 1)
+        self.label_placeholder = QtWidgets.QLabel(self.central_widget)
+        self.label_placeholder.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label_placeholder.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_placeholder.setObjectName("label_placeholder")
+        self.gridLayout.addWidget(self.label_placeholder, 8, 0, 1, 1)
+        self.label_faceattend_icon = QtWidgets.QLabel(self.central_widget)
+        self.label_faceattend_icon.setText("")
+        self.label_faceattend_icon.setPixmap(QtGui.QPixmap("../icons/FaceAttend2.png"))
+        self.label_faceattend_icon.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_faceattend_icon.setObjectName("label_faceattend_icon")
+        self.gridLayout.addWidget(self.label_faceattend_icon, 1, 0, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        self.label_spacer_bottom = QtWidgets.QLabel(self.central_widget)
+        self.label_spacer_bottom.setText("")
+        self.label_spacer_bottom.setObjectName("label_spacer_bottom")
+        self.gridLayout.addWidget(self.label_spacer_bottom, 10, 0, 1, 1)
+        self.label_spacer_middle_2 = QtWidgets.QLabel(self.central_widget)
+        self.label_spacer_middle_2.setText("")
+        self.label_spacer_middle_2.setObjectName("label_spacer_middle_2")
+        self.gridLayout.addWidget(self.label_spacer_middle_2, 7, 0, 1, 1)
+        self.line_edit_username = QtWidgets.QLineEdit(self.central_widget)
+        self.line_edit_username.setMouseTracking(True)
+        self.line_edit_username.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.line_edit_username.setAlignment(QtCore.Qt.AlignCenter)
+        self.line_edit_username.setClearButtonEnabled(True)
+        self.line_edit_username.setObjectName("line_edit_username")
+        self.gridLayout.addWidget(self.line_edit_username, 3, 0, 1, 1)
+        self.button_login = QtWidgets.QPushButton(self.central_widget)
+        self.button_login.setFocusPolicy(QtCore.Qt.StrongFocus)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("../icons/unlock.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.button_login.setIcon(icon2)
+        self.button_login.setObjectName("button_login")
+        self.gridLayout.addWidget(self.button_login, 6, 0, 1, 1)
+        self.label_spacer_middle_1 = QtWidgets.QLabel(self.central_widget)
+        self.label_spacer_middle_1.setText("")
+        self.label_spacer_middle_1.setObjectName("label_spacer_middle_1")
+        self.gridLayout.addWidget(self.label_spacer_middle_1, 2, 0, 1, 1)
+        form_login.setCentralWidget(self.central_widget)
+
+        self.retranslateUi(form_login)
+        QtCore.QMetaObject.connectSlotsByName(form_login)
+        form_login.setTabOrder(self.line_edit_username, self.line_edit_password)
+        form_login.setTabOrder(self.line_edit_password, self.button_login)
+        form_login.setTabOrder(self.button_login, self.button_signup)
+
+    def retranslateUi(self, form_login):
+        _translate = QtCore.QCoreApplication.translate
+        form_login.setWindowTitle(_translate("form_login", "Login"))
+        self.button_signup.setToolTip(_translate("form_login", "Click to get yourself signed up!"))
+        self.button_signup.setText(_translate("form_login", "Signup"))
+        self.line_edit_password.setToolTip(_translate("form_login", "Enter your password."))
+        self.line_edit_password.setPlaceholderText(_translate("form_login", "Password"))
+        self.label_placeholder.setText(_translate("form_login", "Don\'t have access to username/password?\n"
+"Contact Computer Centre or Signup to gain access."))
+        self.line_edit_username.setToolTip(_translate("form_login", "Enter your username."))
+        self.line_edit_username.setPlaceholderText(_translate("form_login", "Username"))
+        self.button_login.setToolTip(_translate("form_login", "Click to Login!"))
+        self.button_login.setText(_translate("form_login", "Login"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    form_login = QtWidgets.QMainWindow()
+    ui = Ui_form_login()
+    ui.setupUi(form_login)
+    form_login.show()
+    sys.exit(app.exec_())
